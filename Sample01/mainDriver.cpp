29a18f5084ed1c2badbb142577272406914c9d9f
Dehkia Samani Khouzani <dehkia.s.k@gmail.com>
2024-09-09 20:21:53 +0330

    Just removing some of the critically private ID's.

--- Sample01/mainDriver.cpp
+++ Sample01/mainDriver.cpp
@@ -1,17 +1,3 @@
-/*
- *
- * Name: Mehrnaz Zarkalam
- * ID: 34677151
- * Teacher: Mehran Vahid
- *
- * Version: 0.1
- * Last update: 22/08/2024
- *
- * Description:
- *          MCD4720 Assignment 2 progress check
- *
- */
-
 #include "Application.h"
 
 
